ae7c989aca859d23028202020509a3cba2a571f5
l4mberjack <frolov_dmitriy06@mail.ru>
2024-11-19 22:09:39 +0300

    доделал пункт 3 и 4 остался выход из программы

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,31 @@
 fun main() {
- println("Этап 1 - Направление поезда")
- val r1 = Route("", "", 0)
- r1.route()
- println()
+  println("Этап 1 - Направление поезда")
+  val r1 = Route()
+   r1.route()
+  println()
 
- println("Этап 2 - Продажа билетов")
- r1.Cashier()
- println()
+  println("Этап 2 - Продажа билетов")
+  r1.Cashier()
+  println()
 
-// println("Этап 3 - Собрать состав")
-// val train = Train()
-// var count = 0
-// var ostatok = r1.tickets
-// while (ostatok != 0) {
-//  train.createWagon()
-//  println("Вагон №${count} вместимостью ${train.seats}")
-//
-//    count++
-// }
+  println("Этап 3 - Собрать состав")
+  val train = Train()
+  var count = 0
+  var ostatok = r1.tickets
+
+  while (ostatok > 0) {
+   train.createWagon()
+   println("Вагон №${count + 1} вместимостью ${train.seats} прицеплен")
+   ostatok -= train.seats
+   count++
+  }
+  println()
+  println("Этап 4 - Отправить поезд")
+  println("Поезд ${r1.startPoint} - ${r1.endPoint}, состоящий из ${count} вагонов отправлен")
+
+ println()
+ println("Хотите продолжить?\n EXIT - выйти из программы \n Иначе - отправить поезд")
+ var choose = readln().toString()
+ if(choose == "EXIT")
+  return
 }
--- src/main/kotlin/Route.kt
+++ src/main/kotlin/Route.kt
@@ -1,18 +1,20 @@
 
-class Route(var startPoint: String, var endPoint: String, var tickets: Int) {
-
+class Route() {
+    var startPoint: String = ""
+    var endPoint: String = ""
+    var tickets: Int = 0
     var listCities = mutableListOf<String>("Омск", "Москва", "СПб", "Иркутск", "Бийск", "Барнаул", "Новосибирск",
-        "Набережные Челны", "Казань", "Калининград", "Великий Новгород", "Пермь", "Архангельская", "Сочи", "Адлер", "Геленджик",
+        "Набережные Челны", "Казань", "Калининград", "Великий Новгород", "Пермь", "Архангельск", "Сочи", "Адлер", "Геленджик",
         "Екатеринбург", "Красноярск", "Мурманск")
 
     fun route () {
-        var start = listCities.random()
-        listCities.remove(start)
-        var end = listCities.random()
-        println("Маршрут: (${start} - ${end})")
+        startPoint = listCities.random()
+        listCities.remove(startPoint)
+        endPoint = listCities.random()
+        println("Маршрут: (${startPoint} - ${endPoint})")
     }
     fun Cashier(){
-        val tickets = (5..201).random()
+         tickets = (5..201).random()
         println("Продано билетов на данный маршрут - ${tickets}")
     }
 }
